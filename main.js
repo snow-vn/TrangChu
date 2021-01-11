3d13a22212cfa41a42635983c77ba37974b79865
AnhTan <https://github.com/snow-vn/TrangChu.git>
2021-01-12 00:54:47 +0700

    AnhTan

---
+++ main.js
@@ -0,0 +1,23 @@
+// ------------------ open and close search ---------------------------
+var formSearch = document.querySelector('.form-search');
+var mainMenu = document.querySelector('.main-nav__center');
+var rightMenu = document.querySelector('.right-menu');
+var searchIcon = document.querySelector('#search-icon');
+var cancelSearch = document.querySelector('#cancel-search');
+searchIcon.addEventListener('click', () =>{
+    openSearch();
+})
+
+cancelSearch.addEventListener('click', () => {
+    closeSearch();
+});
+openSearch = () => {
+    rightMenu.style.display = 'none';
+    mainMenu.style.display = 'none';
+    formSearch.style.display = 'flex';
+}
+closeSearch = () => {
+    rightMenu.style.display = 'block';
+    mainMenu.style.display = 'flex';
+    formSearch.style.display = 'none';
+}
